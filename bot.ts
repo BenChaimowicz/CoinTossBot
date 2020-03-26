a680fef16c2be2cae279ea9b1edc3c8538250dd7
Ben Chaimo <doroza@hotmail.com>
2020-03-26 04:35:26 +0200

    added some behaviors and basic toss action

---
+++ bot.ts
@@ -0,0 +1,43 @@
+import { prefix } from './config.json';
+import * as discord from 'discord.js';
+
+
+interface Message {
+    text: string;
+    image?: string;
+}
+
+interface Behavior {
+    name: string;
+    trigger: string;
+    action?: (msg: discord.Message | discord.PartialMessage) => Message;
+}
+
+
+export class Bot {
+
+    private coinTossed: Message = { text: `Here's a coin for you `, image: 'THCnhJPopHF2o9Cdeb' };
+    public thereYouGo: Message = { text: `Let's see... ah, there you go!`, image: 'ZRMo8QM6M7lM4' };
+    public totalCoins: Message = { text: `Present coin purses!` };
+    public greeting: Message = { text: `Hello there!` };
+    public resetCoins: Message = { text: `All coins tossed!` };
+
+    public behaviors: Behavior[];
+
+    constructor() {
+        this.behaviors = [
+            { name: 'CoinTossed', trigger: prefix + 'toss', action: this.tossCoin },
+            { name: 'NikiHorse', trigger: prefix + 'showniki' },
+            { name: 'Total', trigger: prefix + 'total' },
+            { name: 'Greet', trigger: prefix + 'hello' },
+            { name: 'Reset', trigger: prefix + 'reset' },
+        ]
+    }
+
+    tossCoin = (msg: discord.Message | discord.PartialMessage) => {
+        const member = msg.mentions.members.first();
+        let response: Message = this.coinTossed;
+        response.text = `${response.text} ${member.displayName}`;
+        return response;
+    }
+}
--- index.ts
+++ index.ts
@@ -1,16 +1,30 @@
 import * as discord from 'discord.js';
 import * as config from './config.json';
+import { Bot } from './bot';
 
 const client = new discord.Client();
+const coinTossMsg: string = `Here's a coin for you `;
 
 client.once('ready', () => {
     console.log('Bot running!');
 })
 
 client.on('message', msg => {
-    if (msg.content.startsWith(`${config.prefix}toss`)) {
-        const member = msg.mentions.members.first();
-        msg.channel.send(`Hey ${member.displayName}! Here's a coin!`);
+    if (!msg.content.startsWith(config.prefix)) return;
+
+    const bot: Bot = new Bot();
+    const behavior = bot.behaviors.find(b => msg.content.startsWith(b.trigger));
+
+    if (behavior) {
+        const response = behavior.action(msg);
+        msg.channel.send(response.text);
     }
+    // if (msg.content.startsWith(`${config.prefix}toss`)) {
+    //     const member = msg.mentions.members.first();
+    //     giphy.gifById('THCnhJPopHF2o9Cdeb').then(gifRes => {
+    //         const gif = gifRes.data[0];
+    //         msg.channel.send(finalMsg, { files: [gif] });
+    //     }).catch(err => msg.channel.send(finalMsg))
+    // }
 })
 client.login(config.token);
