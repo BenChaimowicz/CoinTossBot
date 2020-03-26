5e4b95a8b66651df44bf52089dafb6989b3de307
Ben Chaimo <doroza@hotmail.com>
2020-03-26 04:37:25 +0200

    added exclamation mark

--- bot.ts
+++ bot.ts
@@ -16,7 +16,7 @@ interface Behavior {
 
 export class Bot {
 
-    private coinTossed: Message = { text: `Here's a coin for you `, image: 'THCnhJPopHF2o9Cdeb' };
+    private coinTossed: Message = { text: `Here's a coin for you`, image: 'THCnhJPopHF2o9Cdeb' };
     public thereYouGo: Message = { text: `Let's see... ah, there you go!`, image: 'ZRMo8QM6M7lM4' };
     public totalCoins: Message = { text: `Present coin purses!` };
     public greeting: Message = { text: `Hello there!` };
@@ -37,7 +37,7 @@ export class Bot {
     tossCoin = (msg: discord.Message | discord.PartialMessage) => {
         const member = msg.mentions.members.first();
         let response: Message = this.coinTossed;
-        response.text = `${response.text} ${member.displayName}`;
+        response.text = `${response.text} ${member.displayName}!`;
         return response;
     }
 }
